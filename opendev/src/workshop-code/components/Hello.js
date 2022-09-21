9b4763355baba91ad1ec3611ff1bf3d16b550adf
Josh <joshconstantinelim@gmail.com>
2022-09-21 15:34:26 +1000

    useEffect hook

--- opendev/src/workshop-code/components/Hello.js
+++ opendev/src/workshop-code/components/Hello.js
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Hello = () => {
   // let message = "Hello, world!";
 
   // * the useState hook returns an array of size 2 -- the variable's current value and a function to change that variable
   const [message, setMessage] = useState("Hello, world!");
+
+  // * the useEffect will fire on the first render, and then again every time the states given in the dependency array are changed
+
+  useEffect(() => {
+    console.log("first render");
+  }, []); // since the array is empty, the callback only runs on the first render
+
+  useEffect(() => {
+    console.log("message changed");
+  }, [message]); // the callback will run on the first render, and also on any time 'message' is updated
 
   // gets called when the button is clicked
   const handleOnClick = () => {
